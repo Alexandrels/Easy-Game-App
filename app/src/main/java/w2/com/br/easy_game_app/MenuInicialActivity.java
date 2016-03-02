b770eb51664608dbbd1d8bfe7e4a9241106d3db1
alexandre <alexandrels800@gmail.com>
2016-03-02 20:29:40 -0300

    mais uma tela que faltou

--- app/src/main/java/w2/com/br/easy_game_app/MenuInicialActivity.java
+++ app/src/main/java/w2/com/br/easy_game_app/MenuInicialActivity.java
@@ -20,6 +20,7 @@ import w2.com.br.easy_game_app.ui.AgendaUI;
 import w2.com.br.easy_game_app.ui.CadEquipeActivity;
 import w2.com.br.easy_game_app.ui.CadUsuarioActivity;
 import w2.com.br.easy_game_app.ui.MapaUsuariosActivity;
+import w2.com.br.easy_game_app.ui.MeusTimesUI;
 import w2.com.br.easy_game_app.ui.MostraUsuariosActivity;
 
 public class MenuInicialActivity extends AppCompatActivity {
@@ -29,6 +30,7 @@ public class MenuInicialActivity extends AppCompatActivity {
     private Button btnMostraMapaUsuarios;
     private Spinner sp;
     private Button btnAgenda;
+    private Button btnMeusTimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +73,14 @@ public class MenuInicialActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent it = new Intent(v.getContext(), AgendaUI.class);
+                startActivity(it);
+            }
+        });
+        btnMeusTimes = (Button) findViewById(R.id.meusTimes);
+        btnMeusTimes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(v.getContext(), MeusTimesUI.class);
                 startActivity(it);
             }
         });
